72519ea69f36461dbf2b1aabe15a6f36cdeef2dd
Sudipto Paul <asudipto@hotmail.com>
2021-02-02 23:57:15 +0530

    First project checkin
    Shows 1 object on map, along with all fences in the project

---
+++ objects.js
@@ -0,0 +1,151 @@
+var objects = {
+  "type": "FeatureCollection",
+  "features": [{
+      "name": "My Car",
+      "id": "123456",
+      "defaultProject": "22d0b20c-c93d-4410-b20a-852aa6d2f862",
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          73.79112,  //longi
+          18.592   //lati
+        ]
+      },
+      "properties": {
+        "driver": "John Doe",
+        "maxSpeedKmh": 70,
+        "projectName": "Prj 3",
+        "projectId": "22d0b20c-c93d-4410-b20a-852aa6d2f862",
+        "pinCode": 411027,
+        "vehicleType": "Car"
+      }
+    } /*,
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          52.369741,
+          4.891881
+        ]
+      },
+      "properties": {
+        "address": "Rokin 92I 1012 KZ",
+        "city": "Amsterdam"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          52.369070,
+          4.901278
+        ]
+      },
+      "properties": {
+        "address": "Waterlooplein 29A, 1011 NX",
+        "city": "Amsterdam"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          52.371760,
+          4.894093
+        ]
+      },
+      "properties": {
+        "address": "Pieter Jacobszstraat 10D, 1012 HL",
+        "city": "Amsterdam"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          52.372008,
+          4.900800
+        ]
+      },
+      "properties": {
+        "address": "Nieuwmarkt 107, 1011 MA",
+        "city": "Amsterdam"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          51.9202978,
+          4.4787379
+        ]
+      },
+      "properties": {
+        "address": "Coolsingel 105, 3012 AG",
+        "city": "Rotterdam"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          51.915708,
+          4.476373
+        ]
+      },
+      "properties": {
+        "address": "Witte de Withstraat 63 3012 BN",
+        "city": "Rotterdam"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          51.922971,
+          4.468606
+        ]
+      },
+      "properties": {
+        "address": "Weena 691, 3013 AM ",
+        "city": "Rotterdam"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          51.925560,
+          4.483948
+        ]
+      },
+      "properties": {
+        "address": "Goudsesingel 411, 3032 EN",
+        "city": "Rotterdam"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          51.929622,
+          4.472817
+        ]
+      },
+      "properties": {
+        "address": "Paap Dirckstraat 68, 3032 SW",
+        "city": "Rotterdam"
+      }
+    }*/
+  ]
+}
